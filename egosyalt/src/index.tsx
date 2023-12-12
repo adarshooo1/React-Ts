ee7a0f1c7c692fc0ab6a54bfab4c70bfaef6c227
Adarsh Singh <adarshsinghak001@gmail.com>
2023-12-12 23:27:12 +0530

    Documentaion: Added the Basics documentation TS + React

--- egosyalt/src/index.tsx
+++ egosyalt/src/index.tsx
@@ -9,8 +9,28 @@ type Props = {
 
 // Defining a functional component named Greeting
 const Greeting = ({ name }: Props) => {
-  // Rendering a heading element with a dynamic greeting message
+  // Uncomment one of the return statements below based on your preferred syntax
+  // Option 1: JSX syntax
   return <h1>Hello, My name is {name}</h1>;
+
+  // Option 2: Using React.createElement
+  // return React.createElement("h1", { name }, `hi my name is ${name}`);
+
+  // Option 3: JSX syntax with wrapping div
+  // return (
+  //   <div>
+  //     <h1>Hello my name is {name}</h1>
+  //   </div>
+  // );
+
+  // Option 4: Using React.createElement with wrapping div
+  // return React.createElement(
+  //   "div",
+  //   {},
+  //   React.createElement("h1", { name }, `Hello, my name is ${name}`)
+  // );
+
+  //Above is Same but 2 different ways to represent the jsx or tsx. Choose any one as per Easiness.
 };
 
 // Exporting the Greeting component as the default export
